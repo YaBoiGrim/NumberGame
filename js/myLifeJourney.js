319287fb163fe85f2dc811bc27bf66656947c99f
YaBoiGrim <53351932+YaBoiGrim@users.noreply.github.com>
2019-11-13 18:17:13 -0800

    Add files via upload

---
+++ js/myLifeJourney.js
@@ -0,0 +1,35 @@
+function myLifeJourney(){
+  //Array Of Passions
+  var myPassions = ["gaming", "funny", "fixing broken things", "life"];
+  //Array Of Career Options
+  var myCarreerOpts = ["Roblox Developer", "Youtuber", "Engineer"];
+
+  //It Is True That I am Alive
+  var alive = true;
+
+  while (alive) {
+
+getPast9thGrade();
+GetGoodGrades();
+getPast10thGrade();
+GetGoodGrades();
+getPast11thGrade();
+LearnHowToDevelopInRobloxALotBetter();
+goToCollege();
+getA4yearDegree();
+BecomeARobloxDeveloper();
+
+
+      //Check If Still Alive
+      alive = getLifeStatus();
+
+
+
+}
+
+
+function getPast9thGrade(){
+  GoodGrades();
+
+}
+}
